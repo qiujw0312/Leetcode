dbe4e1a057874a603c54973f3aed5f34bb94461a
qiujw0312 <qiujw0312@foxmail.com>
2020-10-07 00:10:46 +0800

    problem 168

---
+++ Python/168-excel-sheet-column-title.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-  
+'''
+给定一个正整数，返回它在 Excel 表中相对应的列名称。
+
+例如，
+
+    1 -> A
+    2 -> B
+    3 -> C
+    ...
+    26 -> Z
+    27 -> AA
+    28 -> AB 
+    ...
+示例 1:
+
+输入: 1
+输出: "A"
+示例 2:
+
+输入: 28
+输出: "AB"
+示例 3:
+
+输入: 701
+输出: "ZY"
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/excel-sheet-column-title
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+'''
+class Solution(object):
+    def convertToTitle(self, n):
+        """
+        :type n: int
+        :rtype: str
+        """
+        list=['Z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y']
+        string = ''
+        while n > 0:
+            i = n % 26
+            string = list[i] + string
+            if i == 0:
+                n -= 26
+            n /= 26
+
+        return string
